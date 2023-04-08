a671c6d81f0c0d2d527c07a36eb438546b8bd17f
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-04-08 14:40:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import './App.css';
-import Home from './components/pages/Home/Home';
+// import Home from './components/pages/Home/Home';
 
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      {/* <h1 className="text-4xl  sm:text-center sm:text-8xl font-extrabold text-transparent  bg-clip-text  bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-blue-600 via-sky-700 to-violet-400 tracking-normal ">
+        Think Different Think Script
+      </h1> */}
+      <div className='flex flex-col justify-center items-center text-center'>
+
+      <h1 className="text-4xl mt-32  sm:text-center sm:text-8xl font-extrabold text-transparent  bg-clip-text  bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-blue-600 via-sky-700 to-violet-400 tracking-normal loose">
+        Building Something Great
+      </h1>
+      <h1 className="text-4xl mt-20   sm:text-center sm:text-8xl font-extrabold text-transparent  bg-clip-text  bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-blue-600 via-sky-700 to-violet-400 tracking-normal loose">
+        Will Back Soon
+      </h1>
+      </div>
+
+
+
     </>
   );
 }
