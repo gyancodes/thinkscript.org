3ca3ab469209c79e8213a101cf375f4561d04cb9
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-05-24 12:34:43 +0530

    removed comment

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Footer from "./components/Footer/Footer";
-// import Navbar from "./components/Navbar/Navbar";
-// import About from "./components/pages/About/About";
-// import Home from "./components/pages/Home/Home";
-// import Contact from "./components/pages/contact/Contact";
-// import Team from "./components/pages/Team/Team";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/pages/About/About";
+import Home from "./components/pages/Home/Home";
+import Contact from "./components/pages/contact/Contact";
+import Team from "./components/pages/Team/Team";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    {/* <Navbar /> */}
+    <Navbar />
 
     <Routes>
       <Route path="/" element={<App />} />
-{/* //       <Route path="/" element={<Home />} />
-//       <Route path="/about" element={<About />} />
-//       <Route path="/contact" element={<Contact />} />
-//       <Route path="/team" element={<Team />} /> */}
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About />} />
+     <Route path="/contact" element={<Contact />} />
+       <Route path="/team" element={<Team />} />
     </Routes>
-    {/* <Footer /> */}
+    <Footer />
   </BrowserRouter>
 );
 
